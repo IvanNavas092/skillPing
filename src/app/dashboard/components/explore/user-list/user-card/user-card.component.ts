2e688030de7ff2f7f25b142350dd442756275e66
IvanNavas092 <navass21112@gmail.com>
2025-05-09 10:50:06 +0200

    implement avatars

--- src/app/dashboard/components/explore/user-list/user-card/user-card.component.ts
+++ src/app/dashboard/components/explore/user-list/user-card/user-card.component.ts
@@ -1,11 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { User } from 'src/app/core/models/User';
+import { AvatarService } from 'src/app/core/services/avatar.service';
 
 @Component({
   selector: 'app-user-card',
   templateUrl: './user-card.component.html',
   styleUrls: ['./user-card.component.css']
 })
-export class UserCardComponent {
+export class UserCardComponent implements OnInit{
   @Input() user!: User;
+
+  constructor(private avatarService: AvatarService, private router: Router) { }
+  ngOnInit(): void {
+    console.log(this.getAvatar());
+    console.log(this.user.avatar);
+  }
+
+  getAvatar() {
+    const avatar = this.avatarService.getAvatarById(this.user.avatar);
+    return avatar ? avatar.img : '/assets/avatar/default_avatar.png'; // Imagen por defecto si no se encuentra el avatar 
+  }
+
+  navigateToUserDetail() {
+    this.router.navigate(['dashboard/explorar/usuario/' + this.user.id]);
+  }
+  
 }
