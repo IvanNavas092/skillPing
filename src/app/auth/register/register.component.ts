91fb993192377288e90f50ff476fa57c871762c8
IvanNavas092 <navass21112@gmail.com>
2025-05-11 16:38:54 +0200

    remove inncesary things

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,3 @@
-import { ThisReceiver } from '@angular/compiler';
 import { Component, ElementRef, OnInit, ViewChild, } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
