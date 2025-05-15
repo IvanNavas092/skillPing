0a3c68df5ec51648be6554ad8d2af637959801a9
IvanNavas092 <navass21112@gmail.com>
2025-05-16 01:09:22 +0200

    fix

--- src/app/dashboard/pages/explore/user-list/user-card/user-card.component.ts
+++ src/app/dashboard/pages/explore/user-list/user-card/user-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from 'src/app/core/models/User';
 import { AvatarService } from 'src/app/core/services/avatar.service';
@@ -8,14 +8,11 @@ import { AvatarService } from 'src/app/core/services/avatar.service';
   templateUrl: './user-card.component.html',
   styleUrls: ['./user-card.component.css']
 })
-export class UserCardComponent implements OnInit{
+export class UserCardComponent  {
   @Input() user!: User;
 
   constructor(private avatarService: AvatarService, private router: Router) { }
-  ngOnInit(): void {
-    console.log(this.getAvatar());
-    console.log(this.user.avatar);
-  }
+
 
   getAvatar() {
     return this.avatarService.getAvatarById(this.user.avatar);
