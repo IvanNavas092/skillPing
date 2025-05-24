1eed697a3174d4246223d3dc3ce2a4b09d667716
IvanNavas092 <navass21112@gmail.com>
2025-05-24 03:23:57 +0200

    comment interceptor

--- src/app/core/csrf.interceptor.ts
+++ src/app/core/csrf.interceptor.ts
@@ -1,34 +1,34 @@
-// csrf.interceptor.ts
-import { Injectable } from '@angular/core';
-import {
-  HttpInterceptor,
-  HttpRequest,
-  HttpHandler,
-  HttpEvent
-} from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { AuthService } from './services/auth.service';
+// // csrf.interceptor.ts
+// import { Injectable } from '@angular/core';
+// import {
+//   HttpInterceptor,
+//   HttpRequest,
+//   HttpHandler,
+//   HttpEvent
+// } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+// import { AuthService } from './services/auth.service';
 
-@Injectable()
-export class CsrfInterceptor implements HttpInterceptor {
-  constructor(private AuthService: AuthService) {}
+// @Injectable()
+// export class CsrfInterceptor implements HttpInterceptor {
+//   constructor(private AuthService: AuthService) {}
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const csrfToken = this.AuthService.getToken();
+//   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+//     const csrfToken = this.AuthService.getToken();
 
-    if (csrfToken) {
-      console.log('token', csrfToken);
-      const cloned = req.clone({
-        setHeaders: {
-          'X-CSRFToken': csrfToken
-        },
-        withCredentials: true
-      });
-      return next.handle(cloned);
-    } else {
-      // Aún no tienes el token, pero aún así necesitas withCredentials
-      const fallback = req.clone({ withCredentials: true });
-      return next.handle(fallback);
-    }
-  }
-}
+//     if (csrfToken) {
+//       console.log('token', csrfToken);
+//       const cloned = req.clone({
+//         setHeaders: {
+//           'X-CSRFToken': csrfToken
+//         },
+//         withCredentials: true
+//       });
+//       return next.handle(cloned);
+//     } else {
+//       // Aún no tienes el token, pero aún así necesitas withCredentials
+//       const fallback = req.clone({ withCredentials: true });
+//       return next.handle(fallback);
+//     }
+//   }
+// }
