71a92b190df6348e965da057480bc4a14be24077
IvanNavas092 <navass21112@gmail.com>
2025-05-24 11:28:22 +0200

    remove the first token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,17 +16,7 @@ export class AppComponent implements OnInit {
   apiUrl = "https://skillping-server.onrender.com/api/";
 
   ngOnInit(): void {
-    this.http
-      .get<{ csrfToken: string }>(`${this.apiUrl}get-csrf-token/`, {
-        withCredentials: true,
-      })
-      .subscribe({
-        next: res => {
-          // save the token in sessionStorage
-          sessionStorage.setItem('csrfToken', res.csrfToken);
-        },
-        error: err => console.error('No pude obtener CSRF token', err),
-      });
+    
     AOS.init();
   }
 }
