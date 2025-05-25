9a8aa87a840eaef2819195172ca3a3d36e376efc
IvanNavas092 <navass21112@gmail.com>
2025-05-25 21:56:02 +0200

    gettoken

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,14 +12,20 @@ import { AuthService } from './core/services/auth.service';
 export class AppComponent implements OnInit {
   title = 'ProyectoTfg_A16_v2';
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService, private http : HttpClient) {
+  }
 
-  apiUrl = "https://skillping-server.onrender.com/api/";
 
   ngOnInit(): void {
+    this.http.get<{ csrfToken: string }>('api/get-csrf-token/', { withCredentials: true })
+  .subscribe(res => sessionStorage.setItem('csrfToken', res.csrfToken));
+
+
     this.auth.checkSession().subscribe({
       next: () => {},
-      error: () => {}
+      error: (error) => {
+        console.log('No esta logueado');
+      }
     });
     AOS.init();
   }
