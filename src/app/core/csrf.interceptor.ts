acc1202e217b8b2ca863af4ca941e18ebf2f7b55
IvanNavas092 <navass21112@gmail.com>
2025-05-25 22:03:21 +0200

    edit interceptor

--- src/app/core/csrf.interceptor.ts
+++ src/app/core/csrf.interceptor.ts
@@ -4,33 +4,22 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpClient
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
 @Injectable()
 export class CsrfInterceptor implements HttpInterceptor {
-  constructor(private http: HttpClient) {}
-
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const mutative = ['POST', 'PUT', 'PATCH', 'DELETE'].includes(req.method);
+    const csrfToken = sessionStorage.getItem('csrfToken');
 
-    if (mutative) {
-      // OBTAIN CSRF TOKEN
-      return this.http
-        .get<{ csrfToken: string }>('/api/get-csrf-token/', { withCredentials: true })
-        .pipe(
-          switchMap(res => {
-            sessionStorage.setItem('csrfToken', res.csrfToken);
-            // 2) SEND AGAIN WITH CSRF TOKEN
-            const clone = req.clone({
-              withCredentials: true,
-              setHeaders: { 'X-CSRFToken': res.csrfToken }
-            });
-            return next.handle(clone);
-          })
-        );
+    if (mutative && csrfToken) {
+      const clone = req.clone({
+        withCredentials: true,
+        setHeaders: { 'X-CSRFToken': csrfToken }
+      });
+      return next.handle(clone);
     }
+
     return next.handle(req.clone({ withCredentials: true }));
   }
 }
